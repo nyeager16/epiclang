72c3e30021e8c2b5f657b5c4a9b7d79e51abb498
Nathan Yeager <nyeager16@gmail.com>
2025-01-27 10:37:31 -0500

    Fix Video width on learn/[word] page

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,9 +1,11 @@
 import pytest
 from django.contrib.auth.models import User
 from django.utils import timezone
+from app.models import (
+    Language, Word, Channel, Video, UserVideo, WatchHistory, WordInstance,
+    UserPreferences, Definition, UserWord, Review
+)
 from datetime import timedelta
-from django.urls import reverse
-from app.models import Language, Word, Video, UserVideoCI, WatchHistory, UserWord, Channel
 
 @pytest.fixture
 def language():
@@ -11,25 +13,39 @@ def language():
     return Language.objects.create(name="Polish", abb="pl")
 
 @pytest.fixture
-def word(language):
-    """Fixture to create a word."""
-    return Word.objects.create(word_text="tak", lang=language)
-
-@pytest.fixture
-def video(language):
-    """Fixture to create a video."""
-    channel = Channel.objects.create(channelurl="http://example.com", channel_name="Example Channel", lastUpdated=timezone.now())
-    return Video.objects.create(url="http://example.com/video1", title="Polish Video", channel=channel, language=language)
-
-@pytest.fixture
 def user():
     """Fixture to create a user."""
     return User.objects.create_user(username='testuser', password='password')
 
 @pytest.fixture
+def word(language):
+    """Fixture to create a word."""
+    return Word.objects.create(word_text="tak", lang=language)
+
+@pytest.fixture
+def channel():
+    """Fixture to create a channel."""
+    return Channel.objects.create(channel_url="http://youtube.com/@test", channel_name="Example Channel")
+
+@pytest.fixture
+def video(language):
+    """Fixture to create a video."""
+    return Video.objects.create(url="http://youtube.com/video1", title="Polish Video", channel=channel, language=language)
+
+@pytest.fixture
 def user_word(user, word):
     """Fixture to create a UserWord."""
-    return UserWord.objects.create(user=user, word=word, needs_review=True)
+    return UserWord.objects.create(user=user, word=word)
+
+@pytest.fixture
+def word_instance(word, video):
+    """Fixture to create a word instance."""
+    return WordInstance.objects.create(word=word, video=video, start="0.1", end="10.2")
+
+@pytest.mark.django_db
+def test_language_creation(language):
+    assert Language.objects.count() == 1
+    assert str(language) == "Polish"
 
 @pytest.mark.django_db
 def test_create_user_word(user, word):
